9ef1a18e27a1b2c87b9daed538a5af50fe9eba0c
n4id4 <117392300+n4id4@users.noreply.github.com>
2023-04-04 17:09:37 +0200

    Update v4-rješenje.cpp

--- v4-rješenje.cpp
+++ v4-rješenje.cpp
@@ -1,273 +1,219 @@
 #include<iostream>
-#include <string.h>
-
 using namespace std;
-//Koristene skracenice u komentarima
-// dflt. = default
-// user-def. = user-defined (korisnicki-definirano)
-// ctor = constructor (konstruktor)
-// copy ctor = copy constructor (konstruktor kopije)
 
-//Z1.0
-char* alocirajTekst(const char* tekst) {
-    //Implementirati funkciju
-    if(tekst==nullptr)
-        return nullptr;
-    int duzina = strlen(tekst) + 1;
-    char* temp = new char[duzina];
-    strcpy_s(temp, duzina, tekst);
-    return temp;
-}
+class Datum {
+	int _dan, _mjesec, * _godina = nullptr;
 
-class Datum
-{
-    int* _dan=nullptr;
-    int* _mjesec{ nullptr };
-    int* _godina{ nullptr };
 public:
-    //Z1.1 Dflt. ctor
-  /*  Datum() {
-        setDan(1);
-        setMjesec(1);
-        setGodina(2023);
-    }*/
-    //Z1.2 User-def. ctor
-    Datum(int d=1, int m=1, int g=2023) {
-        //Implementirati funkciju
-        setDan(d);
-        setMjesec(m);
-        setGodina(g);
-    }
+	void Unos(int d, int m, int g)
+	{
+		SetDan(d);
+		SetMjesec(m);
+		SetGodina(g);
+	}
+	void Unos(Datum& datum)
+	{
+		SetDan(datum.GetDan());
+		SetMjesec(datum.GetMjesec());
+		SetGodina(datum.GetGodina());
+	}
+	void Unos(Datum* datum)
+	{
+		SetDan(datum->GetDan());
+		//SetDan((*datum).GetDan());
+		SetMjesec(datum->GetMjesec());
+		SetGodina(datum->GetGodina());
+	}
 
-    Datum(const Datum& obj)
-    {
-        setDan(obj.getDan());
-        setMjesec(obj.getMjesec());
-        setGodina(obj.getGodina());
-    }
-    //Z1.3
-    int getDan() const {
-        //Implementirati funkciju 
-        if (_dan == nullptr)
-            return 0;
-        return *_dan;
-    } 
-    int getMjesec() const { 
-        //Implementirati funkciju
-        if(_mjesec==nullptr)
-            return 0;
-        return *_mjesec;
-    }  
-    int getGodina() const { 
-        //Implementirati funkciju
-        if(_godina==nullptr)
-            return 0; 
-        return *_godina;
-    }  
-    //Z1.4 
-    void setDan(int dan) {
-        if (_dan == nullptr)
-            _dan = new int;
-        *_dan = dan;
-    }
-    void setMjesec(int mjesec) {
-        //Implementirati funkciju
-        if (_mjesec == nullptr)
-            _mjesec = new int;
-        *_mjesec = mjesec;
-    }
-    void setGodina(int godina) {
-        //Implementirati funkciju
-        if (_godina == nullptr)
-            _godina = new int;
-        *_godina = godina;
-    }
-  
-    //Z1.6
-    void ispis() const {
-        //Implementirati funkciju
-        cout << getDan() << "." << getMjesec() << "." << getGodina() << endl;
-    }
-    //Z1.7
-    ~Datum() {
-        //Implementirati funkciju
-        delete _dan;
-        //_dan = nullptr;
-        delete _mjesec;
-        //_mjesec = nullptr;
-        delete _godina;
-        //_godina = nullptr;
-        _dan = _mjesec = _godina = nullptr;
-    }
+	int GetDan() { return _dan; }
+
+	int GetMjesec() { return _mjesec; }
+
+	int GetGodina() {
+		if (_godina == nullptr)
+			return 2023;
+		return *_godina;
+	}
+
+	void SetDan(int dan) { _dan = dan; }
+
+	void SetMjesec(int mjesec) { _mjesec = mjesec; }
+
+	void SetGodina(int godina)
+	{
+		if (_godina == nullptr)
+			_godina = new int;
+		*_godina = godina;
+	}
+	void Ispis()
+	{
+		cout << _dan << "." << _mjesec << "." << *_godina;
+	}
+	void Dealociraj()
+	{
+		delete _godina;
+		_godina = nullptr;
+	}
 };
+
 class Zanr {
-    char* _naziv;
-    char _opis[200];
+	char _naziv[40];
+	char _opis[200]; 
 public:
-    //Z2.1 Dflt. ctor
-    Zanr() {
-        //implementirati funkciju
-        setNaziv("");
-        setOpis("");
-    }
-    //Z2.2 user-def. ctor
-    Zanr(const char* naziv, const char* opis)
-    {
-        //implementirati funkciju
-        setNaziv(naziv);
-        setOpis(opis);
-    }
-    //Z2.3 copy ctor
-    Zanr(const Zanr& obj)
-    {
-        setNaziv(obj.getNaziv());
-        setOpis(obj.getOpis());
-    }
-    //Z2.6
-    void Ispis() {
-        //implementirati funkciju
-        cout << "Naziv zanra: " << getNaziv() << endl;
-        cout << "Opis zanra: " << getOpis() << endl;
-    }
-    //Z2.5 destruktor
-    ~Zanr()
-    {
-        //implementirati funkciju
-        delete[] _naziv;
-        _naziv = nullptr;
-    }
-
-    char* getNaziv() const { return _naziv; }
-    const char* getOpis() const { return _opis; }
-    void Unos(Zanr* zanr)
-    {
-        strcpy_s(_naziv, strlen(zanr->_naziv) + 1, zanr->_naziv);
-        strcpy_s(_opis, strlen(zanr->_opis) + 1, zanr->_opis);
-    }
-    void setNaziv(const char* naziv) { _naziv = alocirajTekst(naziv);}
-
-    void setOpis(const char* opis) { strcpy_s(_opis, strlen(opis) + 1, opis);}
+		void Unos(const char* naziv, const char* opis)
+		{
+			strcpy_s(_naziv, strlen(naziv) + 1, naziv);
+			strcpy_s(_opis, strlen(opis) + 1, opis);
+		}
+		void Unos(Zanr& zanr)
+		{
+			strcpy_s(_naziv, strlen(zanr._naziv) + 1, zanr._naziv);
+			strcpy_s(_opis, strlen(zanr._opis) + 1, zanr._opis);
+		}
+		void Unos(Zanr* zanr)
+		{
+			strcpy_s(_naziv, strlen(zanr->_naziv) + 1, zanr->_naziv);
+			strcpy_s(_opis, strlen(zanr->_opis) + 1, zanr->_opis);
+		}
+		void Ispis()
+		{
+			cout << "Naziv zanra: " << _naziv << endl;
+			cout << "Opis: " << _opis << endl;
+		}
+		void Dealociraj() {}
 };
 
 class Knjiga {
-    char* _naziv;
-    char _autor[50];
-    Zanr* _zanr;
+	char _naziv[40];
+	char _autor[50];
+	Zanr* _zanr;
 public:
-    //Z3.1 dflt. ctor
-    Knjiga()
-    {
-        //implementirati funkciju
-        _naziv = alocirajTekst("");
-        strcpy_s(_autor, strlen("") + 1, "");
-    }
-
-    //Z3.2 user-def. ctor
-    Knjiga(const char* naziv, const char* autor, Zanr& zanr) {
-        //implementirati funkciju
-        _naziv = alocirajTekst(naziv);
-        strcpy_s(_autor, strlen(autor) + 1, autor);
-        _zanr = new Zanr(zanr);
-    }
-
-    //Z3.3 copy ctor
-    Knjiga(const Knjiga& knjiga) {
-        //implementirati funkciju
-        _naziv = alocirajTekst(knjiga._naziv);
-        strcpy_s(_autor, strlen(knjiga._autor) + 1, knjiga._autor);
-        _zanr = new Zanr(*knjiga._zanr);
-    }
-
-    //Z3.4
-    void Ispis() {
-        //implementirati funkciju
-        cout << "Autor: " << _autor << endl;
-        cout << "Naziv knjige: " << _naziv << endl;
-        _zanr->Ispis();
-    }
-
-    //Z3.5
-    ~Knjiga() {
-        //implementirati funkciju
-        delete[] _naziv;
-        _naziv = nullptr;
-        delete _zanr;
-        _zanr = nullptr;
-    }
-
-    void Unos(Knjiga& knjiga)
-    {
-        strcpy_s(_naziv, strlen(knjiga._naziv) + 1, knjiga._naziv);
-        strcpy_s(_autor, strlen(knjiga._autor) + 1, knjiga._autor);
-        _zanr = new Zanr();
-        _zanr->Unos(knjiga._zanr);
-    }
+	void Unos(const char* naziv, const char* autor, Zanr zanr)
+	{
+		strcpy_s(_naziv, strlen(naziv) + 1, naziv);
+		strcpy_s(_autor, strlen(autor) + 1, autor);
+		_zanr = new Zanr;
+		_zanr->Unos(zanr);
+	}
+	void Unos(Knjiga& knjiga)
+	{
+		strcpy_s(_naziv, strlen(knjiga._naziv) + 1, knjiga._naziv);
+		strcpy_s(_autor, strlen(knjiga._autor) + 1, knjiga._autor);
+		_zanr = new Zanr();
+		_zanr->Unos(knjiga._zanr);
+	}
+	void Unos(Knjiga* knjiga)
+	{
+		strcpy_s(_naziv, strlen(knjiga->_naziv) + 1, knjiga->_naziv);
+		strcpy_s(_autor, strlen(knjiga->_autor) + 1, knjiga->_autor);
+		_zanr = new Zanr();
+		_zanr->Unos(knjiga->_zanr);
+	}
+	void Ispis()
+	{
+		cout << "Naziv knjige: " << _naziv << endl;
+		cout << "Autor: " << _autor << endl;
+		_zanr->Ispis();
+	}
+	void Dealociraj()
+	{
+		_zanr->Dealociraj();
+		delete _zanr;
+		_zanr = nullptr;
+	}
 };
+char* AlocirajTekst(const char* tekst)
+{
+	if (tekst == nullptr)
+		return nullptr;
+	int duzina = strlen(tekst) + 1;
+	char* temp = new char[duzina];
+	strcpy_s(temp, duzina, tekst);
+	return temp;
+}
 
 class Biblioteka {
-    char* _naziv;
-    char* _adresa;
-    Knjiga* _nizKnjiga;
-    int _trenutnoKnjiga;
+	char* _naziv;
+	char* _adresa;
+	Knjiga* _nizKnjiga;
+	int _trenutnoKnjiga;
 public:
-    //Z4.1 dflt. ctor
-    Biblioteka()
-    {
-        //implementirati funkciju
-        _naziv = alocirajTekst("");
-        _adresa = alocirajTekst("");
-        _nizKnjiga = nullptr;
-        _trenutnoKnjiga = 0;
-    }
-    //Z4.2 user-def. ctor
-    Biblioteka(const char* naziv, const char* adresa)
-    {
-        _naziv = alocirajTekst(naziv);
-        _adresa = alocirajTekst(adresa);
-        _nizKnjiga = nullptr;
-        _trenutnoKnjiga = 0;
-    }
+	void Unos(const char* naziv, const char* adresa)
+	{
+		_naziv = AlocirajTekst(naziv);
+		_adresa = AlocirajTekst(adresa);
+		_nizKnjiga = nullptr;
+		_trenutnoKnjiga = 0;
+	}
+	void Ispis()
+	{
+		cout << "Naziv biblioteke: " << _naziv << endl;
+		cout << "Adresa: " << _adresa << endl;
+		cout << "Knjige: " << endl;
+		for (int i = 0; i < _trenutnoKnjiga; i++)
+		{
+			_nizKnjiga[i].Ispis();
+			cout << "------------------------------------------------------------------------------------------------------------------------\n";
+		}
+		cout << "Trenutno knjiga: " << _trenutnoKnjiga << endl;
 
-    //Z4.3
-    void Ispis() {
-        //implementirati funkciju
-        cout << "Naziv biblioteke: " << _naziv << endl;
-        cout << "Adresa biblioteke: " << _adresa << endl;
-        for (int i = 0; i < _trenutnoKnjiga; i++)
-            _nizKnjiga[i].Ispis();
-        cout << "Trenutno knjiga: " << _trenutnoKnjiga << endl;
-    }
+	}
 
-    bool DodajKnjigu(Knjiga k) {
-        //implementirati funkciju
-        Knjiga* temp = _nizKnjiga;
+	bool DodajKnjigu(Knjiga k) {
+		Knjiga* temp = _nizKnjiga;
 		_nizKnjiga = new Knjiga[_trenutnoKnjiga + 1];
 		for (int i = 0; i < _trenutnoKnjiga; i++)
 			_nizKnjiga[i] = temp[i];
-
-		delete[] temp;
-		temp = nullptr;
-
 		_nizKnjiga[_trenutnoKnjiga++].Unos(k);
 		return true;
-    }
+	}
+	void Dealociraj()
+	{
+		delete[] _naziv, delete[] _adresa;
+		_naziv = _adresa = nullptr;
 
-    ~Biblioteka() {
-        //implementirati funkciju
-        delete[] _naziv;
-        _naziv = nullptr;
-        delete[] _adresa;
-        _adresa = nullptr;
-        delete[] _nizKnjiga;
-        _nizKnjiga = nullptr;
-    }
+		for (int i = 0; i < _trenutnoKnjiga; i++)
+			_nizKnjiga[i].Dealociraj();
+
+		delete[] _nizKnjiga;
+		_nizKnjiga = nullptr;
+		_trenutnoKnjiga = 0;
+	}
 };
 
 int main() {
-    Datum datum(1, 1, 2023);
+	const char* crt = "-------------------------------------------------------------------------\n\n";
+	Datum d1;
+	d1.Unos(28, 3, 2023);
 
-    Datum* d2 = new Datum(datum);
+	Zanr* zanrovi = new Zanr[3];
+	zanrovi[0].Unos("Realizam", " Realisticku metodu oznacava knjizevnost koja prikazuje stvarnost kakvom je, ali i kakva je moguca prema zakonu vjerojatnosti i nuznosti, odnosno knjizevnost koja je uvjerljiva u odnosu prema objektivnoj stvarnosti.");
+	zanrovi[1].Unos("Komedija", "Komedija je vrsta drame koja de odlikuje veselim sadrzajem.");
+	zanrovi[2].Unos("Tragedija", "Tragedija je vrsta drame koja se bazira na negativnim dogadjajima sa tragicnim ishodom.");
 
-    system("pause>0");
-    return 0;
+	Knjiga antigona, aulularia;
+	antigona.Unos("Antigona", "Sofoklo", zanrovi[2]);
+	aulularia.Unos("Aulularia", "Plaut", zanrovi[1]);
+
+
+	Biblioteka b;
+	b.Unos("Narodna biblioteka Mostar", "Marsala Tita 55");
+
+	b.DodajKnjigu(antigona);
+	b.DodajKnjigu(aulularia);
+
+	b.Ispis();
+
+	d1.Dealociraj();
+
+	antigona.Dealociraj();
+	aulularia.Dealociraj();
+
+	b.Dealociraj();
+	for (int i = 0; i < 3; i++)
+		zanrovi[i].Dealociraj();
+	delete[] zanrovi;
+
+	system("pause>0");
+	return 0;
 }
